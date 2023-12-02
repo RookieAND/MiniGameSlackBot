9a103b7e8668564b39dcfa67e5991a7d2f06f659
Baik Gwangin <gwangin1999@naver.com>
2023-12-02 22:35:12 +0900

    feat: Slack App Token 추가 및 앱 실행 Config에 적용

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { App as SlackBotApp } from '@slack/bolt';
 import dotenv from 'dotenv';
 
-const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET } = process.env;
-
 dotenv.config();
+
+const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN } = process.env;
 
 const slackBotApp = new SlackBotApp({
   token: SLACK_BOT_TOKEN,
   signingSecret: SLACK_SIGNING_SECRET,
+  appToken: SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
 slackBotApp
   .start(process.env.APP_PORT || 3000)
-  .then(() => console.log('Slack Bot Started!'))
-  .catch(() => console.error('An Error is occurred while launching Slack Bot'));
+  .then(() => console.log('Slack Bot Started!'));
