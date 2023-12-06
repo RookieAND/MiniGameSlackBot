c63d9f6977588e64efbbb1c1e08c1f5ed7a818da
RookieAND <gwangin1999@naver.com>
2023-12-07 00:51:43 +0900

    feat: action, shortcut 핸들러 app 부팅 시 추가

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import { App as SlackBotApp } from '@slack/bolt';
+import { App } from '@slack/bolt';
 import dotenv from 'dotenv';
+
+import { handleAddSelectOption, handleRemoveSelectOption } from '@/action';
+import { registerVoteShortCut } from '@/command';
 
 dotenv.config();
 
 const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN } = process.env;
 
-const slackBotApp = new SlackBotApp({
+export const slackBotApp = new App({
   token: SLACK_BOT_TOKEN,
   signingSecret: SLACK_SIGNING_SECRET,
   appToken: SLACK_APP_TOKEN,
   socketMode: true,
 });
 
-slackBotApp
-  .start(process.env.APP_PORT || 3000)
-  .then(() => console.log('Slack Bot Started!'));
+(async () => {
+  await slackBotApp.start();
+  handleAddSelectOption();
+  handleRemoveSelectOption();
+  registerVoteShortCut();
+})();
